12785fe6aaa6fbc8a352180f4f73ff50aa09c6eb
ardavanshamroshan <ardavan2000.ashr@gmail.com>
2025-02-18 22:28:38 +0330

    episode 4: added: UserResource / modified: AdminPanelProvider with 'vazirmatn' font

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,27 +2,29 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
     protected $fillable = [
-       'name',
-       'email',
-       'email_verified_at',
-       'mobile',
-       'mobile_verified_at',
-       'password',
-       'role',
-       'avatar_url',
-       'ip',
-       'agent',
-       'status',
-       'remember_token',
+        'name',
+        'email',
+        'email_verified_at',
+        'mobile',
+        'mobile_verified_at',
+        'password',
+        'role',
+        'avatar_url',
+        'ip',
+        'agent',
+        'status',
+        'remember_token',
     ];
 
     protected $hidden = [
@@ -33,9 +35,14 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            'email_verified_at'  => 'datetime',
             'mobile_verified_at' => 'datetime',
-            'password'          => 'hashed',
+            'password'           => 'hashed',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role == 'admin';
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name'  => 'Ardavan Shamroshan',
+            'email' => 'admin@test.com',
+            'role'  => 'admin',
         ]);
+
+        User::factory(5)->create();
     }
 }
